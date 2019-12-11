cd828cf7912209910bd00462a0809c98d0ba39f1
FLYmaker <FLYmaker@users.noreply.github.com>
2019-12-11 17:46:12 +0800

    1

--- 固件/Marlin/Configuration.h
+++ 固件/Marlin/Configuration.h
@@ -130,7 +130,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_FLYF407ZG
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
